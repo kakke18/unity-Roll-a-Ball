e112ff86e41fe547aa61e162a77052ff4d407556
kakke18 <tanaka-kakeru-xm@ynu.jp>
2018-06-21 13:04:51 +0900

    Fix GameController.cs

--- Assets/Script/GameController.cs
+++ Assets/Script/GameController.cs
@@ -5,7 +5,7 @@ public class GameController : MonoBehaviour {
 	public UnityEngine.UI.Text scoreLabel;
 
 	public void Update () {
-		int count = GameObject.FindGameObjectsWithTag("item").Length;
+		int count = GameObject.FindGameObjectsWithTag("Item").Length;
 		scoreLabel.text= count.ToString();
 	}
 }
