cb07f399dcf9e34491807a292078054d6a01b0ae
kakke18 <tanaka-kakeru-xm@ynu.jp>
2018-06-21 13:17:11 +0900

    Add game clear judgement

--- Assets/Script/GameController.cs
+++ Assets/Script/GameController.cs
@@ -3,9 +3,14 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour {
 	public UnityEngine.UI.Text scoreLabel;
+	public GameObject winnerLabelObject;
 
 	public void Update () {
 		int count = GameObject.FindGameObjectsWithTag("Item").Length;
 		scoreLabel.text= count.ToString();
+
+		if (count == 0) {
+			winnerLabelObject.SetActive(true);
+		}
 	}
 }
